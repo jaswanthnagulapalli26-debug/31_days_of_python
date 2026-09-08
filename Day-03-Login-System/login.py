fd575c4084cec92eba755f03ecff452f83bf3740
Jaswanth Sai <jaswanthnagulapalli26@gmail.com>
2026-09-08 18:34:12 +0530

    Day 3 - Login System

---
+++ Day-03-Login-System/login.py
@@ -0,0 +1,8 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "jaswanth" and password == "6986":
+    print("Login Successful")
+    print("Welcome!")
+else:
+    print("Wrong username or password")
