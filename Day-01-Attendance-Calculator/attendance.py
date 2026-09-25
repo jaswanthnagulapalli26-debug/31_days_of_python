de1bc6f9b55dc5fc941ccf1dbdaa3021b12ba675
jaswanthnagulapalli26-debug <jaswanthnagulapalli26@gmail.com>
2026-09-25 14:34:14 +0530

    Delete Day-01-Attendance-Calculator/attendance.py

--- Day-01-Attendance-Calculator/attendance.py
+++
@@ -1,8 +0,0 @@
-total = int(input("Enter total classes: "))
-attended = int(input("Enter attended classes: "))
-percentage = (attended / total) * 100
-print("Attendance:", percentage, "%")
-if percentage >= 75:
-    print("Eligible")
-else:
-    print("Not Eligible")
